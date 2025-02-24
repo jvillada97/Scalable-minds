e1cb266e7378fe7f8b95bc3d0f46a7e259cd061d
LuciaColorado <m.colorado@uniandes.edu.co>
2025-02-24 12:49:18 -0500

    Correccion Mensajes

--- src/app/seedwork/infraestructura/schema/v1/mensajes.py
+++ src/app/seedwork/infraestructura/schema/v1/mensajes.py
@@ -1,10 +1,8 @@
 import uuid
 
 from pulsar.schema import *
-from app.seedwork.infraestructura.utils import time_millis
-from app.modulos.imagen_medica.dominio.entidades import ImagenMedica
 
 
-class Mensaje(ImagenMedica):
+class Mensaje(Record):
     id = String(default=str(uuid.uuid4()))   
     url_imagen = String()
