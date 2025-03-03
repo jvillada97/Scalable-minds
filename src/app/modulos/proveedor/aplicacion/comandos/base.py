4b26aefc8981b938122c105742063e53c9210f0c
Juan Camilo <j.villadap@uniandes.edu.co>
2025-03-03 18:29:55 -0500

    se agrega archivo de fabricas de codigo

---
+++ src/app/modulos/proveedor/aplicacion/comandos/base.py
@@ -0,0 +1,17 @@
+from app.seedwork.aplicacion.comandos import ComandoHandler
+from app.modulos.proveedor.infraestructura.fabricas import FabricaRepositorio
+from app.modulos.proveedor.dominio.fabricas import FabricaProveedor
+
+class CrearProveedorBaseHandler(ComandoHandler):
+    def __init__(self):
+        self._fabrica_repositorio: FabricaRepositorio = FabricaRepositorio()
+        self._fabrica_proveedor: FabricaProveedor = FabricaProveedor()
+
+    @property
+    def fabrica_repositorio(self):
+        return self._fabrica_repositorio
+    
+    @property
+    def fabrica_proveedor(self):
+        return self._fabrica_proveedor    
+    
