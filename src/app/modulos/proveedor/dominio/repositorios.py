bff3880c41fd09600b11324be7a667b3b71474e2
Juan Camilo <j.villadap@uniandes.edu.co>
2025-03-03 18:39:17 -0500

    se agrega archivo de repositorios

---
+++ src/app/modulos/proveedor/dominio/repositorios.py
@@ -0,0 +1,17 @@
+""" Interfaces para los repositorios del dominio de vuelos
+
+En este archivo usted encontrará las diferentes interfaces para repositorios
+del dominio de vuelos
+
+"""
+
+from abc import ABC
+from app.seedwork.dominio.repositorios import Repositorio
+
+class RepositorioProveedors(Repositorio, ABC):
+    ...
+
+
+class RepositorioEventosProveedors(Repositorio, ABC):
+    ...
+
