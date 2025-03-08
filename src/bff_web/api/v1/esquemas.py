8326b76f52039d84f0a87b76126c957034aa4f48
Mateo Castaño <m.castano1@uniandes.edu.co>
2025-03-08 16:16:02 +0000

    adding new endpoints

--- src/bff_web/api/v1/esquemas.py
+++ src/bff_web/api/v1/esquemas.py
@@ -9,10 +9,14 @@ from datetime import datetime
 
 FORMATO_FECHA = '%Y-%m-%dT%H:%M:%SZ'
 
-def obtener_image(root) -> typing.List["Reserva"]:
-    imagenes_medicas = requests.get(f'http://localhost:5001/imagen-medica').json()
-    return [ImagenMedica(**imagen) for imagen in imagenes ]
-    
+def obtener_image(root) -> typing.List["ImagenMedica"]:
+    response = requests.get(f'https://5001-jvillada97-scalablemind-5s4upqot80m.ws-us118.gitpod.io/imagen-medica')
+    imagenes_medicas = []
+    for imagenes in response:
+        imagenes_medicas.append(ImagenMedica(
+            data=imagenes
+        ))
+    return imagenes_medicas
 
 @strawberry.type
 class ImagenMedica:
